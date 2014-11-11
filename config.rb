59ceb4df6d53d9422baaf81adacc54fed069dcd8
kamem <kamei_masatoshi_gn@cyberagent.co.jp>
2014-11-11 12:14:17 +0900

    add public

---
+++ config.rb
@@ -0,0 +1,9 @@
+require "./rb/skyward_design.rb"
+require "./rb/develo.rb"
+
+http_path = "/"
+css_dir = "public/css/"
+sass_dir = "public/sass"
+images_dir = "img/"
+javascripts_dir = "js/"
+relative_assets = true
